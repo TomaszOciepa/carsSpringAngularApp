6fe533ef281d18b458bd2f745e92a1255fb68e7f
tomaszociepa <tom.ociepa@gmail.com>
2019-12-04 13:05:15 +0100

    first commit

---
+++ src/main/java/pl/tom/carsspringangular/CarsSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package pl.tom.carsspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsSpringAngularApplication.class, args);
+    }
+
+}
