f6a8ed0313287a7e7135e75c7dad68340106d827
tomaszociepa <tom.ociepa@gmail.com>
2019-12-04 23:02:15 +0100

    creating the getCar method on the server

--- server/src/main/java/pl/tom/carsspringangular/cars/CarController.java
+++ server/src/main/java/pl/tom/carsspringangular/cars/CarController.java
@@ -2,7 +2,10 @@ package pl.tom.carsspringangular.cars;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 
 @RestController
@@ -18,5 +21,11 @@ public class CarController {
     @CrossOrigin(origins = "http://localhost:4200")
     public Iterable<Car> getAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/car")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Optional<Car> getCar(@RequestParam(value = "id", required = false) Long id ){
+        return repository.findById(id);
     }
 }
--- server/src/main/java/pl/tom/carsspringangular/cars/CarRepo.java
+++ server/src/main/java/pl/tom/carsspringangular/cars/CarRepo.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepo extends CrudRepository<Car, Long> {
+
 }
